a3a627e7d61b48a33abf32c45c15f29a9ec6b370
Jerzy J. Gangi <jerzygangi@gmail.com>
2017-05-29 02:45:21 -0500

    Allow Excel spreadsheet to be built from dataframes (#1)
    * Created initial version of XLSBuilder
    * Created Readme with documentation on XLSBuilder and logo
    * Git is now ignoring .DS_Store files

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Forklift: a Big Data Toolkit
+![Forklift](./artwork/logo_rendered.png)
+
+# Welcome to Forklift
+If you're building a 21st century data pipeline on Hadoop, and orchestrating it with Airflow, you'll quickly become overwhelmed. How do you move a table from Redshift to HDFS? How do you clean up data from S3? How do you convert from TSV to Parquet? How do you quickly rename columns? Forklift solves all of these problems. It's the first ETL ("Extract-Transform-Load") suite for Hadoop and Airflow. So sit back, clone the repo, and let Forklift do the heavy lifting.
+
+## What can Forklift do?
+### Make an Excel file from dataframes
+XLSBuilder builds an Excel file, with a tab for each Spark dataframe:
+
+```python
+xls = XLSBuilder("my_file.xls")
+df1 = sqlContext.read.parquet("hdfs:///data1")
+xls.addTab(df1, "Data 1")
+df2 = sqlContext.read.parquet("hdfs:///data2")
+xls.addTab(df2, "Data 2")
+xls.build()
+```
+
+N.B. XLSBuilder collects and writes dataframes on the master node of your Spark cluster. If you attempt to use large dataframes with XLSBuilder, undefined behavior may occur. This is not considered a bug, since desktop usage of Excel is itself limited to small, `n MB` .xls files. Therefore, XLSBuilder should be used with small dataframes.
---
+++ utilities/xls_builder.py
@@ -0,0 +1,30 @@
+#
+# XLSBuilder
+# Make an Excel file from dataframes
+#
+# N.B. XLSBuilder collects and writes dataframes on the master node of your Spark cluster. If you attempt to use large dataframes with XLSBuilder, undefined behavior may occur. This is not considered a bug, since desktop usage of Excel is itself limited to small, `n MB` .xls files. Therefore, XLSBuilder should be used with small dataframes.
+#
+
+import xlsxwriter
+
+class XLSBuilder():
+  def __init__(self, path):
+    self.workbook = xlsxwriter.Workbook(path)
+  def addTab(self, df, tab_name):
+    # Step 1: Make a new tab in Excel
+    this_tab = self.workbook.add_worksheet(tab_name)
+    # Step 2: Write the column headers
+    columns = df.columns
+    for column_index, column in enumerate(columns):
+      this_tab.write_string(0, column_index, column)
+    # Step 3: Turn the dataframe into a local Python array in memory on the master node
+    df_as_local_python_array = df.collect()
+    # Step 4: Add each row from the dataframe into the Excel spreadsheet
+    # We do this by iterating over each row, and then iterating over each column,
+    # so as to not rely on the implicit ordering of columns in each Row object
+    for row_index, row in enumerate(df_as_local_python_array):
+      row_number = row_index + 1 # Accout for header row added above in Step 2
+      for column_index, column in enumerate(columns):
+        this_tab.write(row_number, column_index, row[column])
+  def write(self):
+    self.workbook.close()
