7403d50161971d30d3df2e6521f6224dc62b31cb
Jerzy J. Gangi <jerzygangi@gmail.com>
2017-06-02 20:51:02 -0700

    Change normalize_and_sanitize method signature to make it clear the first argument refers to the @classmethod nature

--- lib/forklift/__init__.py
+++ lib/forklift/__init__.py
@@ -1,10 +1,11 @@
 from os.path import isfile
 from json import load
+from pyspark.sql.dataframe import DataFrame
 
 class Forklift(object):
 
   @classmethod
-  def normalize_and_sanitize(self, dataframe, with_spark_schema, remappings_file_path, cast_processor):
+  def normalize_and_sanitize(klass, dataframe, with_spark_schema, remappings_file_path, cast_processor):
     print("Step 1: Check that arguments are valid")
     if not isinstance(dataframe, Dataframe):
       raise TypeError("dataframe must be an instance of Dataframe")
@@ -33,5 +34,5 @@ class Forklift(object):
     caster = Forklift.Caster(cast_processor, with_spark_schema)
     dataframe_with_columns_renamed_and_unwanted_columns_deleted_and_types_cast_and_schema_applied = caster.cast(dataframe_with_columns_renamed_and_unwanted_columns_deleted)
 
-    print("Step 4: Return the normalized and sanitized dataframe")
+    print("Step 5: Return the normalized and sanitized dataframe")
     return dataframe_with_columns_renamed_and_unwanted_columns_deleted_and_types_cast_and_schema_applied
