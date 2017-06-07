abac1ebfe798471c6b080c8c4a85f1400159e623
Jerzy J. Gangi <jerzygangi@gmail.com>
2017-06-06 17:27:54 -0700

    Created classes for Stage flags

---
+++ lib/forklift/stages.py
@@ -0,0 +1,14 @@
+class ForkliftStage(object):
+  pass
+
+class NS_ALL(ForkliftStage):
+  pass
+
+class NS_RENAME_COLS(ForkliftStage):
+  pass
+
+class NS_DELETE_COLS(ForkliftStage):
+  pass
+
+class NS_CAST_CELLS(ForkliftStage):
+  pass
