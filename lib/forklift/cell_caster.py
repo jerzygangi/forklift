2dce29e9c10aefa100e94014c51da63b42196a83
Jerzy J. Gangi <jerzygangi@gmail.com>
2017-06-06 22:03:16 -0700

    Fixed typo in caster

--- lib/forklift/cell_caster.py
+++ lib/forklift/cell_caster.py
@@ -22,5 +22,5 @@ class CellCaster(object):
     return casted_row
 
   def cast(self, dataframe):
-    cast_rdd dataframe.rdd.map(lambda row: self.cast_values_in_row(row))
+    cast_rdd = dataframe.rdd.map(lambda row: self.cast_values_in_row(row))
     return sqlContext.createDataFrame(cast_rdd, self.schema)
