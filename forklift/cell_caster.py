59c15f1a34903fe4b4185b2d707e29485feafeb9
Jerzy J. Gangi <jerzygangi@gmail.com>
2017-06-08 19:20:48 -0700

    Warmer…

--- forklift/cell_caster.py
+++ forklift/cell_caster.py
@@ -1,5 +1,4 @@
 import copy
-from .rdd_cell_caster import cast_values_in_row
 
 # Parent class from which to inherit your cast processors
 class CastProcessor(object):
@@ -13,5 +12,23 @@ class CellCaster(object):
 
   def cast(self, dataframe):
     distributed_safe_cast_processor_klass = copy.deepcopy(self.cast_processor_klass)
-    cast_rdd = dataframe.rdd.map(lambda row: cast_values_in_row(row, distributed_safe_cast_processor_klass))
+    distributed_safe_this_klass = copy.deepcopy(self.__class__)
+    cast_rdd = dataframe.rdd.map(lambda row: distributed_safe_this_klass.cast_values_in_row(row, distributed_safe_cast_processor_klass))
     return self.sql_context.createDataFrame(cast_rdd, self.schema)
+
+  @classmethod
+  def cast_cell(klass, column_name, old_cell_value, cast_processor_klass):
+    cast_processor = cast_processor_klass()
+    if hasattr(cast_processor, 'cast_all_cells'):
+      old_cell_value = cast_processor.cast_all_cells(old_cell_value)
+    if hasattr(cast_processor, 'cast_{0}'.format(column_name)):
+      params=(old_cell_value,)
+      return getattr(cast_processor, 'cast_{0}'.format(column_name))(*params)
+    else:
+      return old_cell_value
+
+  @classmethod
+  def cast_values_in_row(klass, row, cast_processor_klass):
+    uncast_row = row.asDict()
+    casted_row = {column_name: klass.cast_cell(column_name, cell_value, cast_processor_klass) for column_name, cell_value in uncast_row.items()}
+    return casted_row
