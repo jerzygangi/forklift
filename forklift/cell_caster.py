f573ee17324f4ffce6554b0bde306fc5ac01faab
Jerzy J. Gangi <jerzygangi@gmail.com>
2017-06-08 17:59:52 -0700

    Attempting to avoid CellCaster from getting pickled

--- forklift/cell_caster.py
+++ forklift/cell_caster.py
@@ -2,29 +2,27 @@
 class CastProcessor(object):
   pass
 
+def cast_cell(column_name, old_cell_value, cast_processor_klass):
+  cast_processor = cast_processor_klass()
+  if hasattr(cast_processor, 'cast_all_cells'):
+    old_cell_value = cast_processor.cast_all_cells(old_cell_value)
+  if hasattr(cast_processor, 'cast_{0}'.format(column_name)):
+    params=(old_cell_value,)
+    return getattr(cast_processor, 'cast_{0}'.format(column_name))(*params)
+  else:
+    return old_cell_value
+
+def cast_values_in_row(row):
+  uncast_row = row.asDict()
+  casted_row = {column_name: cast_cell(column_name, cell_value, cast_processor_klass) for column_name, cell_value in uncast_row.items()}
+  return casted_row
+
 class CellCaster(object):
   def __init__(self, cast_processor_klass, spark_schema, sql_context):
     self.cast_processor_klass = cast_processor_klass
     self.schema = spark_schema
     self.sql_context = sql_context
 
-  @classmethod
-  def cast_cell(klass, column_name, old_cell_value, cast_processor_klass):
-    cast_processor = cast_processor_klass()
-    if hasattr(cast_processor, 'cast_all_cells'):
-      old_cell_value = cast_processor.cast_all_cells(old_cell_value)
-    if hasattr(cast_processor, 'cast_{0}'.format(column_name)):
-      params=(old_cell_value,)
-      return getattr(cast_processor, 'cast_{0}'.format(column_name))(*params)
-    else:
-      return old_cell_value
-
-  @classmethod
-  def cast_values_in_row(klass, row, cast_processor_klass):
-    uncast_row = row.asDict()
-    casted_row = {column_name: klass.cast_cell(column_name, cell_value, cast_processor_klass) for column_name, cell_value in uncast_row.items()}
-    return casted_row
-
   def cast(self, dataframe):
-    cast_rdd = dataframe.rdd.map(lambda row: self.__class__.cast_values_in_row(row, self.cast_processor_klass))
+    cast_rdd = dataframe.rdd.map(lambda row: cast_values_in_row(row))
     return self.sql_context.createDataFrame(cast_rdd, self.schema)
