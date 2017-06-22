a295c2080b76abae4675fee10cf38032ea503a84
Jerzy J. Gangi <jerzygangi@gmail.com>
2017-06-22 13:19:04 -0700

    Created an adapter for reading and writing DSV files to/from HDFS and S3

---
+++ forklift/adapters/dsv.py
@@ -0,0 +1,48 @@
+import .utilities.options_checker
+from .adapters import Adapter
+from .exceptions import CantReadUsingThisAdapterException, CantWriteUsingThisAdapterException
+
+class DSVAdapter(Adapter):
+
+  @ensure_required_options_exist(["delimiter", "has_header", "delimiter", "url", "select_query", "table_name_in_select_query"])
+  def read(self, sql_context, **kwargs):
+    options = kwargs["options"]
+
+    # Try to use this adapter
+    try:
+      print("Step 1: The delimiter for a DSV file must be comma or tab")
+      if options["delimiter"] not in [",", "\t"]:
+        raise CantLoadUsingThisAdapterException 
+      print("Step 2: Read in the DSV file as a temporary table")
+      sql_context.read \
+        .format('com.databricks.spark.csv') \
+        .options(header=options["has_header"], delimiter=options["delimiter"], inferSchema="true", dateFormat="yyyy-MM-dd'T'HH:mm:ss") \
+        .load(options["url"]) \
+        .registerTempTable(options["table_name_in_select_query"])
+      print("Step 3: Make a DataFrame by running the select query on the DSV's temporary table")
+      dataframe = sqlContext.sql(options["select_query"])      
+      print("Step 4: Drop the temporary table of the DSV file")
+      sqlContext.dropTempTable(options["table_name_in_select_query"])      
+      print("Step 5: Return the queried DSV file as a DataFrame")
+      return dataframe
+    # If it bombs for any reason, skip it!
+    except:
+      print("WARNING: Could not load this DSV into a DataFrame")
+      raise CantReadUsingThisAdapterException
+
+  @ensure_required_options_exist(["output_mode", "url"])
+  def write(self, dataframe, **kwargs):
+    options = kwargs["options"]
+
+    # Try to use this adapter
+    try:
+      print("Step 1: Write out the DSV file")
+      df.write \
+        .format('com.databricks.spark.csv') \
+        .mode(options["output_mode"]) \
+        .save(options["url"])
+      return
+    # If it bombs for any reason, skip it!
+    except:
+      print("WARNING: Could not save this DataFrame to a DSV file")
+      raise CantWriteUsingThisAdapterException
