568f2367a67c7b4cbba984f34b30b90eaeec4919
Jerzy J. Gangi <jerzygangi@gmail.com>
2017-06-22 13:33:38 -0700

    Created an adapter for reading and writing Redshift tables to/from DataFrames

---
+++ forklift/adapters/redshift.py
@@ -0,0 +1,42 @@
+import .utilities.options_checker
+from .adapters import Adapter
+from .exceptions import CantReadUsingThisAdapterException, CantWriteUsingThisAdapterException
+
+class RedshiftAdapter(Adapter):
+
+  @ensure_required_options_exist(["jdbc_connection_string", "sql_select_query", "s3_temp_directory"])
+  def read(self, sql_context, **kwargs):
+    options = kwargs["options"]
+
+    # Try to use this adapter
+    try:
+      print("Step 1: Read the Redshift query into a DataFrame")
+      return sql_context.read \
+        .format("com.databricks.spark.redshift") \
+        .option("url", options["jdbc_connection_string"]) \
+        .option("query", options["sql_select_query"]) \
+        .option("tempdir", options["s3_temp_directory"]) \
+        .load()
+    # If it bombs for any reason, skip it!
+    except:
+      print("WARNING: Could not load this Redshift query into a DataFrame")
+      raise CantReadUsingThisAdapterException
+
+  @ensure_required_options_exist(["jdbc_connection_string", "table_name", "s3_temp_directory", "output_mode"])
+  def write(self, dataframe, **kwargs):
+    options = kwargs["options"]
+
+    # Try to use this adapter
+    try:
+      print("Step 1: Write out the Redshift table")
+      return dataframe.write \
+        .format("com.databricks.spark.redshift") \
+        .option("url", options["jdbc_connection_string"]) \
+        .option("dbtable", options["table_name"]) \
+        .option("tempdir", options["s3_temp_directory"]) \
+        .mode(options["output_mode"]) \
+        .save()
+    # If it bombs for any reason, skip it!
+    except:
+      print("WARNING: Could not save this Redshift table to a DataFrame")
+      raise CantWriteUsingThisAdapterException
