c32c495199166e3194191df1ccfdd035c7829f37
Jerzy J. Gangi <jerzygangi@gmail.com>
2017-06-22 18:28:58 -0700

    Bumped version now that exceptions are present

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='forklift',
-    version='0.2.10',
+    version='0.2.11',
     author='Jerzy J. Gangi',
     author_email='jerzy@jerzygangi.com',
     packages=['forklift'],
