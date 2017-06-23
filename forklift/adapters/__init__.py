99150b37d9b9a5a1d20dd0089640a5bbad0528b6
Jerzy J. Gangi <jerzygangi@gmail.com>
2017-06-22 19:47:57 -0700

    Root module of Adapters now imports everything for you

--- forklift/adapters/__init__.py
+++ forklift/adapters/__init__.py
@@ -1,4 +1,8 @@
 from ..utilities.options_checker import *
+from .dsv import DSVAdapter
+from .parquet import ParquetAdapter
+from .postgresql import PostgreSQLAdapter
+from .redshift import RedshiftAdapter
 from ..exceptions import CantReadUsingThisAdapterException, CantWriteUsingThisAdapterException
 
 # An "Adapter" is a way of reading data into spark.
