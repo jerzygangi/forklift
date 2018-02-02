318989a518991ebc531ca0c8678907945cbb3b4b
Jerzy J. Gangi <jerzygangi@gmail.com>
2018-02-01 23:07:27 -0800

    Allow PostgreSQL via JDBC to autoparition based on a calculated stride from the requested query

--- forklift/adapters/postgresql.py
+++ forklift/adapters/postgresql.py
@@ -22,18 +22,37 @@ class PostgreSQLAdapter(Adapter):
       except StringIsNotAFileException:
         select_query_as_string = options["sql_select_query"]
         print("WARNING: The sql_select_query was identified as a SQL string")
+      print("Step 2: Prepare the JDBC connection to PostgreSQL")
+      postgres_jdbc_options = {}
+      postgres_jdbc_options["url"] = options["jdbc_connection_string"]
+      postgres_jdbc_options["user"] = options["username"]
+      postgres_jdbc_options["password"] = options["password"]
+      postgres_jdbc_options["driver"] = "org.postgresql.Driver"
+      postgres_jdbc_options["mode"] = "error"
       # For executing a query on a JDBC connection, view syntax instructions:
       # 1) https://docs.databricks.com/spark/latest/data-sources/sql-databases.html#pushdown-an-entire-query
       # 2) http://stackoverflow.com/questions/34365692/spark-sql-load-data-with-jdbc-using-sql-statement-not-table-name
-      print("Step 2: Read the PostgreSQL query into a DataFrame")
+      print("Step 3: If partitioning was requested, calculate partitioning for the JDBC connection")
+      if(options["partition_column"] and isinstance(options["partition_column"], (str, unicode)):
+        minimum, maximum, count = sql_context.read \
+          .format("jdbc") \
+          .options(**postgres_jdbc_options) \
+          .option("dbtable", "(SELECT min({partition_column}) AS minimum_pk, max({partition_column}) AS maximum_pk, count(*) AS count_pk FROM ({query}) rltion) AS tmp".format(partition_column=options["partition_column"], query=select_query_as_string)) \
+          .load()
+          .collect()[0]
+        import math
+        fetch_size = 10000
+        computed_partitions = int(math.ceil(count*1.0/fetch_size*1.0))
+        postgres_jdbc_options["fetchsize"] = fetch_size
+        postgres_jdbc_options["lowerBound"] = minimum
+        postgres_jdbc_options["upperBound"] = maximum
+        postgres_jdbc_options["partitionColumn"] = options["partition_column"]
+        postgres_jdbc_options["numPartitions"] = computed_partitions
+      print("Step 4: Read the PostgreSQL query into a DataFrame")
       return sql_context.read \
         .format("jdbc") \
-        .option("url", options["jdbc_connection_string"]) \
+        .options(**postgres_jdbc_options) \
         .option("dbtable", "({0}) AS tmp".format(select_query_as_string)) \
-        .option("user", options["username"]) \
-        .option("password", options["password"]) \
-        .option("mode", "error") \
-        .option("driver", "org.postgresql.Driver") \
         .load()
     # If it bombs for any reason, skip it!
     except Exception as e:
