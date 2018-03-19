1580d94995aee965cc77c043a38b1eec8fd989a6
Emmanuel Job <emmanuel.job@accuenmedia.com>
2018-03-19 16:11:50 +0000

    add dependency and bump version

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@ from distutils.core import setup
 
 setup(
     name='forklift',
-    version='0.6',
+    version='0.6.1',
     author='Jerzy J. Gangi',
     author_email='jerzy@jerzygangi.com',
     packages=['forklift'],
     scripts=[],
     url='http://jerzygangi.com',
     description='ETL for Spark and Airflow',
+    install_requires=[
+        'xlsxwriter',
+        ]
 )
